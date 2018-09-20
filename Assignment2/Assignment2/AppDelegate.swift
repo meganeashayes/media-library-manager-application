24e462c5368971897032339aa3360f714c8af09e
sweta7 <anvita1982@gmail.com>
2018-09-20 21:25:36 +1200

    About page working.

--- Assignment2/Assignment2/AppDelegate.swift
+++ Assignment2/Assignment2/AppDelegate.swift
@@ -21,7 +21,9 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     @IBOutlet weak var imageView: NSImageView!
     @IBOutlet weak var outletscroll: NSScrollView!
     @IBOutlet weak var aboutWindow: NSView!
+
     @IBOutlet weak var aboutWindowText: NSTextField!
+    
     @IBOutlet weak var searchTab: NSSearchField!
     @IBOutlet weak var searchForward: NSButton!
     @IBOutlet weak var searchBackward: NSButton!
@@ -30,7 +32,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     @IBOutlet weak var fitButton: NSButton!
     @IBOutlet weak var lastPageButton: NSButton!
     @IBOutlet weak var nextPageButton: NSButton!
-   
+    
     
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
@@ -61,22 +63,23 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         let aboutSize: NSSize = CGSize(width: 200, height: 250)
         aboutWindow.window?.setIsVisible(false)
         aboutWindow.setFrameSize(aboutSize)
-        aboutWindowText.stringValue = "Media Colection Library"
+        aboutWindowText.stringValue = "This is application to manage media collection.\n Developed by Sweta Kumari and Megan Hayes created for the University of Otago, COSC-346 Assignment2."
         
     }
     
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
+   
     
     @IBAction func openAboutWindow(_ sender: Any) {
     aboutWindow.window?.setIsVisible(true)
+    aboutWindow.window?.setTitleWithRepresentedFilename("About")
     }
     
     
     @IBAction func zoomIn(_ sender: NSButton) {
-        library.zoomIn(sender)
+        //library.zoomIn(sender)
     }
     
     
