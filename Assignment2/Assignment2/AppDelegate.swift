7d85e213757e47ff2ad2f33ae23c4c83eb779755
Sweta Kumari <skumari@oucs1443.otago.ac.nz>
2018-09-26 12:49:43 +1200

    added code from Paper's resource page

--- Assignment2/Assignment2/AppDelegate.swift
+++ Assignment2/Assignment2/AppDelegate.swift
@@ -7,18 +7,19 @@
 //
 
 import Cocoa
-import MediaLibrary
+import AVKit
+import AVFoundation
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-    //var playView = AVPlayer();
-    //var soundPlayer =  AVAudioPlayer();
+    var playView = AVPlayer();
+    var soundPlayer =  AVAudioPlayer();
     @IBOutlet weak var window: NSWindow!
-    
     var library: Collection = Collection()
     var last = MMResultSet()
     @IBOutlet var outletTextView: NSTextView!
-    @IBOutlet weak var imageView: NSImageView!
+    @IBOutlet weak var outletImage: NSImageView!
+    @IBOutlet weak var outletVideo: AVPlayerView!
     @IBOutlet weak var outletscroll: NSScrollView!
     @IBOutlet weak var aboutWindow: NSView!
 
@@ -33,22 +34,33 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     @IBOutlet weak var lastPageButton: NSButton!
     @IBOutlet weak var nextPageButton: NSButton!
     
+    @IBAction func play(_ sender: NSButtonCell) {
+        print("playing") ;
+//        soundPlayer.play();
+        playView.play();
+        
+    }
+    
     
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        // print("test");
+        
         let bundlePath = Bundle.main.resourcePath
-        // print(bundlePath!) ;
-        imageView.image=NSImage(contentsOfFile: bundlePath!+"/test.png")
         
         
+         print(bundlePath!) ;
+        outletImage.image = NSImage(contentsOfFile: bundlePath!+"/home/cshome/s/skumari/346/test.png")
+
+
         if let filepath = Bundle.main.path(forResource: "readme", ofType: "txt"){
-            
+
             do {
                 let contents = try String(contentsOfFile: filepath)
                 // print(contents)
                 outletTextView.string = contents;
                 outletTextView.isEditable = false ;
-                
+
             } catch {
                 // contents could not be loaded
             }
@@ -56,15 +68,29 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             print ("else");
         }
         
-        outletscroll.isHidden=false;
-        imageView.isHidden = false;
         
-        // about window reagrding app
+        let fileURL = NSURL(fileURLWithPath: "/home/cshome/s/skumari/346/test.mov");
+        playView = AVPlayer(url: fileURL as URL);
+        outletVideo.player = playView ;
+        print("video")
+        
+//        do {
+//            soundPlayer = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "test", ofType: "m4a")!))
+//            soundPlayer.prepareToPlay();
+//            print("playing")
+//        } catch {
+//        }
+        
+        outletscroll.isHidden=false;
+        outletVideo.isHidden=false;
+        outletImage.isHidden = false;
+        
+        // about window regarding app
         let aboutSize: NSSize = CGSize(width: 200, height: 250)
         aboutWindow.window?.setIsVisible(false)
         aboutWindow.setFrameSize(aboutSize)
         aboutWindowText.stringValue = "This is application to manage media collection.\n Developed by Sweta Kumari and Megan Hayes created for the University of Otago, COSC-346 Assignment2."
-        
+
     }
     
     func applicationWillTerminate(_ aNotification: Notification) {
