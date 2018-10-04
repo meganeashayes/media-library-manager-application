d594a00b26986ad219e7c8ed843a4bb964e7c90f
Sweta Kumari <skumari@oucs1527.otago.ac.nz>
2018-10-04 17:15:21 +1300

    working on search string

--- Assignment2/Assignment2/DisplayController.swift
+++ Assignment2/Assignment2/DisplayController.swift
@@ -80,4 +80,27 @@ class DisplayController: NSWindowController {
         }
     }
     
+    /*
+    * Function to seach String in textield
+     */
+    func getString(title: String, question: String, defaultValue: String) -> String {
+        let msg = NSAlert()
+        msg.addButton(withTitle: "OK")      // 1st button
+        msg.addButton(withTitle: "Cancel")  // 2nd button
+        msg.messageText = title
+        msg.informativeText = question
+        
+        let txt = NSTextField(frame: NSRect(x: 0, y: 0, width: 200, height: 24))
+        txt.stringValue = defaultValue
+        
+        msg.accessoryView = txt
+        let response: NSApplication.ModalResponse = msg.runModal()
+        
+        if (response == NSApplication.ModalResponse.alertFirstButtonReturn) {
+            return txt.stringValue
+        } else {
+            return ""
+        }
+    }
+    
 }
