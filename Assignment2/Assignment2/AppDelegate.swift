24907100f4a3c999ebbb69452e0c51cab1a2798c
Sweta Kumari <skumari@oucs1528.otago.ac.nz>
2018-10-05 18:39:18 +1300

    Final update

--- Assignment2/Assignment2/AppDelegate.swift
+++ Assignment2/Assignment2/AppDelegate.swift
@@ -14,7 +14,7 @@ import Quartz
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate, NSSearchFieldDelegate {
     
-    /// Outlets linked to main window
+    ///Outlets linked to main window
     @IBOutlet weak var window: NSWindow!
     @IBOutlet var outletTextView: NSTextView!
     @IBOutlet weak var outletImage: NSImageView!
@@ -32,50 +32,31 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSSearchFieldDelegate {
     @IBOutlet weak var outletTextFileView: NSTextView!
     
     
-    //Local variables can make it internal???????????????????????????????
+    //Local variables
     var isSearching = false
     var filterData = [String]()
     var playView = AVPlayer()
     var soundPlayer =  AVAudioPlayer()
     var last = MMResultSet()
-    
     let aboutWindow = aboutPageController()
     @objc dynamic var mediaFiles = NSMutableArray()
     
-    //searchBar.delegate = self
     
-    /// When application launches, the media containers (outlets), metadata display and media notes
+    /// When application launches, the media containers (outlets), metadata display and media notes.
     /// are hidden automatically; these will display only once a media file is selected to be displayed.
     /// Files are imported into the library automatically on application launch.
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
         self.window?.setTitleWithRepresentedFilename("Media Library App")
         
-        //let bundlePath = Bundle.main.resourcePath
         outletTextView.isHidden = true
         outletscroll.isHidden = true
         outletNotes.isHidden = true
         outletVideo.isHidden = true
         outletImage.isHidden = true
         outletTextFileView.isHidden = true
-        
         let importer = ImportFiles()
         importer.importMediaFiles(mediaFiles: mediaFiles)
-        
-//        if let filepath = Bundle.main.path(forResource: "readme", ofType: "txt"){
-//            
-//            do {
-//                let contents = try String(contentsOfFile: filepath)
-//                outletTextView.string = contents
-//                outletTextView.isEditable = false
-//                
-//            } catch {
-//                print("Contents could not be loaded")
-//            }
-//        } else {
-//            print ("Filepath could not be found")
-//        }
-        
     }
     
     func applicationWillTerminate(_ aNotification: Notification) {
@@ -83,7 +64,6 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSSearchFieldDelegate {
     }
     
     
-   
     
     /**** FUNCTION ASSOCIATED WITH THE MAIN WINDOW OBJECT ****/
     
@@ -94,19 +74,6 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSSearchFieldDelegate {
         aboutWindow.window?.setIsVisible(true)
     }
     
-    
-//    @IBAction func openNewApp(_ sender: Any) {
-//        [Bundle.loadXibNamed:"SecondaryWindow", owner: self.window]
-//        
-//    }
-    
-    
-    /// Zooms out the view of the current image on clicking + button
-    /// - Parameters:
-    ///     - sender: the - (zoom out) button
-    @IBAction func zoomOut(_ sender: NSButton) {
-        outletImage.scaleUnitSquare(to: NSSize(width: 0.75, height: 0.75))
-    }
     
     /// Imports files into the media library
     /// - Parameters:
@@ -131,59 +98,31 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSSearchFieldDelegate {
         
     }
     
-
-
-    /// Zooms in the view of the current media
+    /// Zooms out the view of the current image on clicking + button
+    /// - Parameters:
+    ///     - sender: the - (zoom out) button
+    @IBAction func zoomOut(_ sender: NSButton) {
+        outletImage.scaleUnitSquare(to: NSSize(width: 0.75, height: 0.75))
+    }
+    
+    /// Zooms in the view of the current media on clicking - button
     /// - Parameters:
     ///     - sender: NSButton
-    /// Zooms in the view of the current image on clicking - button
     @IBAction func zoomIn(_ sender: NSButton) {
-        //        var t = CGAffineTransform.identity
-        //        t = t.translatedBy(x: -100, y: -300)
-        //        t = t.scaledBy(x: 2, y: 2)
-        //
-        //        outletImage.layer?.setAffineTransform(t)
-        //        outletImage.layer?.setAffineTransform(CGAffineTransform.identity)
         outletImage.scaleUnitSquare(to: NSSize(width: 1.25, height: 1.25))
-        //        let size:NSSize = outletImage.bounds.size
-        //
-        //        let newSize: NSSize = NSMakeSize(size.width * 0.9, size.height * 0.9)
-        //
-        //        outletImage.setBoundsSize(newSize)
-    }
-
-    
-    @IBAction func zoomFit(_ sender: NSButton) {
-        //let size:NSSize = outletImage.image!.size
-        //print(size)
-        //outletImage.setBoundsSize(size)
-       //outletImage.sizeToFit()
-        //print("line 146 zoomFit")
         
-//                var t = CGAffineTransform.identity
-//                t = t.translatedBy(x: -100, y: -300)
-//                t = t.scaledBy(x: 2, y: 2)
-//
-//                outletImage.layer?.setAffineTransform(t)
-//                outletImage.layer?.setAffineTransform(CGAffineTransform.identity)
-//                let size:NSSize = outletImage.bounds.size
-//
-//                let newSize: NSSize = NSMakeSize(size.width * 0.9, size.height * 0.9)
-//
-//                outletImage.setBoundsSize(newSize)
-        //let rect = AVMakeRectWithAspectRatioInsideRect(image.size, imageView.bounds)
-        //let size:NSSize = outletImage.bounds.size
-        //let newSize: NSSize = NSMakeSize(1, 1)
-        //outletImage.resizeSubviews(withOldSize: newSize)
-        outletImage.layer?.setAffineTransform(CGAffineTransform.identity)
     }
     
-    /// Enables media (image/video etc.) to be displayed in the application when the media name is clicked in the table
-    ///
+    /// Zooms fit in the view of the current media
+    /// - Parameters:
+    ///     - sender: NSButton
+    @IBAction func zoomFit(_ sender: NSButton) {
+        //outletImage.layer?.setAffineTransform(CGAffineTransform.identity)
+    }
+    
+    /// Enables media (image/video etc.) to be displayed in the application when the media name is clicked in the table.
     /// Uses regular expressions to identify the kind of media to display, and thus which media container to show on the screen.
-    /// Displays the metadata associated with the file in a TextView below the media container, and hides the media container
-    /// which is not in use at the time
-    ///
+    /// Displays the metadata associated with the file in a TextView below the media container, and hides the media container which is not in use at the time
     /// - Parameters:
     ///     - sender: the media item clicked on the table
     @IBAction func tableViewAction(_ sender: Any) {
@@ -217,7 +156,6 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSSearchFieldDelegate {
                 outletVideo.player = playView
                 outletImage.isHidden = true
                 outletTextFileView.isHidden = true
-                //outletImage.image = NSImage(contentsOfFile: file.fullpath)
             } else if (regexAudio.firstMatch(in: file.filename, options: [], range: range) != nil) {
                 outletVideo.isHidden = false
                 let fileURL = NSURL(fileURLWithPath: file.fullpath)
@@ -238,10 +176,7 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSSearchFieldDelegate {
                     outletVideo.isHidden = true
                     outletImage.isHidden = true
                 } catch {
-                    
                 }
-                //let contents = try NSString(contentsOfFile: file.path, encoding: String.Encoding.utf8.rawValue)
-                //outletTextFileView.string = contents as String
                 outletTextView.string = "Document information:\n"
                 outletTextView.string.append("\(file.metadata[0])\n")
                 outletVideo.isHidden = true
@@ -250,9 +185,6 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSSearchFieldDelegate {
         } else {
             outletTextView.string = "File doesn't exist"
         }
-        
     }
-    
-    
     
 }
